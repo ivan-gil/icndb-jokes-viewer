9abcd7047d1a5a79a93a31722ba4427e2b048375
ivan-gil <Ivan_Hil@epam.com>
2018-08-12 22:55:42 +0300

    [CN-3] - fix major issue with npm run express
    - change entry point for express server

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, './../dist')));
+app.use('/', express.static(path.join(__dirname, './../prod')));
 app.listen(3000, () => console.log('express app listening on port 3000!'));
